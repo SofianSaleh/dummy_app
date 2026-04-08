883ce3d35ce8879355cc0cc487d1a71e1d6ccc0c
SofianSaleh <sofiane.saleh11@gmail.com>
2026-04-08 14:33:50 +0200

    [Feat]: added dummy endpoints

--- api.py
+++ api.py
@@ -2,8 +2,10 @@ import frappe
 
 @frappe.whitelist(allow_guest=False, methods=["POST"])
 def get_hello_world():
-    return "Hello World"
+  frappe.db.error_log('Webhook Triggered 1')
+  return "Hello World"
 
 @frappe.whitelist(allow_guest=True, methods=["POST"])
 def create_user():
-    return 'User Created'
+  frappe.db.error_log('Webhook Triggered 2')
+  return 'User Created'
